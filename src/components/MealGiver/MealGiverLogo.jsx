73e24739c86f7ec73c1e544c023e2957767d7a76
ismail-dev-code <hm.ismail772@gmail.com>
2025-07-07 20:14:17 +0600

    feat: font montserrat + color setup

---
+++ src/components/MealGiver/MealGiverLogo.jsx
@@ -0,0 +1,15 @@
+
+import { GiMeal } from "react-icons/gi";
+
+const MealGiverLogo = () => {
+  return (
+    <div className="flex items-center gap-2 select-none">
+      <GiMeal className="text-4xl text-primary" />
+      <span className="text-2xl sm:text-3xl font-bold tracking-tight text-gray-800">
+        <span className="text-primary">Meal</span>Giver
+      </span>
+    </div>
+  );
+};
+
+export default MealGiverLogo;
